89d078bcc6f43c3ea8047c3a88fba17511850eb1
Tomer <tomerb93@gmail.com>
2019-10-04 18:23:24 +0300

    End of section 4

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
-import { Header, Icon, List } from 'semantic-ui-react'
+import { Header, Icon, List } from 'semantic-ui-react';
 import './App.css';
 import axios from 'axios';
 
 class App extends Component {
   state = {
     values: []
-  }
+  };
 
   componentDidMount() {
-    axios.get('http://localhost:5000/api/values')
-      .then((response) => {
-        this.setState({
-          values: response.data
-        });
+    axios.get('http://localhost:5000/api/values').then((response) => {
+      console.log(response);
+      this.setState({
+        values: response.data
       });
+    });
   }
 
   render() {
